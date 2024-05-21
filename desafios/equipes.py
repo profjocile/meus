cb82f3cf49d891a8f4e59cd22a40f17d955d021b
ProfJocile <profjocile@gmail.com>
2024-05-21 16:22:24 -0300

    Desafio de montar equipes refatorado

--- desafios/equipes.py
+++ desafios/equipes.py
@@ -3,25 +3,16 @@ Faça um algoritmo para cadastrar os alunos que estão se canditando para equipe
 '''
 
 # Monte 4 equipes de alunos
-equipe1 = []
-equipe2 = []
-equipe3 = []
-equipe4 = []
+equipe = [[],[],[],[]]
 
 # Lista de nomes de alunos
 
 nomes = set (['Caio','Davi', 'Edmilson', 'Erick', 'Esmael', 'Alison', 'Expedito', 'Armando', 'Kauã', 'Marcos', 'Graziel', 'Raiane', 'Natanael', 'Gabriel', 'Pedro', 'Silas'])
 
-for i in range(4):
-    equipe1.append(nomes.pop())
-    equipe2.append(nomes.pop())
-    equipe3.append(nomes.pop())
-    equipe4.append(nomes.pop())
+for j in range(4):
+  for i in range(4): 
+    equipe[i].append(nomes.pop())
 
 # Mostre os nomes por cada equipe
-print(f'''
-      Equipe 1: {equipe1}\n
-      Equipe 2: {equipe2}\n
-      Equipe 3: {equipe3}\n
-      Equipe 4: {equipe4}\n
-      ''')
+for i in range(4): 
+  print(f'Equipe {i + 1}: {equipe[i]}\n')
